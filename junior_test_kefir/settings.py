2a4a8b131f97926912cbf5d006558eb6b1b81b1e
Inna Misovetc <inna.misovets@skoltech.ru>
2022-01-29 16:57:48 +0300

    Add IsAuthenticated permission as default

--- junior_test_kefir/settings.py
+++ junior_test_kefir/settings.py
@@ -130,3 +130,12 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
