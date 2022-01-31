4b43c76f01208a87a002fea6224f8154af3535e8
Inna Misovetc <inna.misovets@skoltech.ru>
2022-01-31 23:36:45 +0300

    Add password hashing for CreateUserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from .models import CustomUser
+from django.contrib.auth.hashers import make_password
 
 
 class BasicUserSerializer(serializers.ModelSerializer):
@@ -27,6 +28,10 @@ class CreateUserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ('id', 'first_name', 'last_name', 'other_name', 'email', 'phone', 'birthday', 'city', 'additional_info', 'is_admin', 'password')
         extra_kwargs = {'password': {'write_only': True}, 'id': {'read_only': True}}
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super(CreateUserSerializer, self).create(validated_data)
 
 
 class PrivatePatchUserSerializer(serializers.ModelSerializer):
